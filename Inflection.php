63ddaef63b65e92576e9c25e32fe748d16226c1e
Pedro Miguel <pedro.aos19@gmail.com>
2020-09-17 14:38:40 +0100

    docs

--- Inflection.php
+++ Inflection.php
@@ -4,6 +4,20 @@ namespace Pantheion\Facade;
 
 use Pantheion\Utils\Inflection as InflectionRootClass;
 
+/**
+ * @method static string camelize(\string $string)
+ * @method static string classerize(\string $string)
+ * @method static string functionize(\string $string)
+ * @method static string humanize(\string $string)
+ * @method static string hyphenize(\string $string)
+ * @method static string lowercase(\string $string)
+ * @method static string pluralize(\string $string)
+ * @method static string singularize(\string $string)
+ * @method static string tablerize(\string $string)
+ * @method static string titleize(\string $string)
+ * @method static string underscorize(\string $string)
+ * @method static string uppercase(\string $string)
+ */
 class Inflection extends Facade
 {
     protected static function accessor()
--- Table.php
+++ Table.php
@@ -4,6 +4,14 @@ namespace Pantheion\Facade;
 
 use Pantheion\Database\Table\Manager as ManagerRootClass;
 
+/**
+ * @method static \Pantheion\Database\Table\Table create(string $table, \Closure $schematic)
+ * @method static boolean exists(string $table)
+ * @method static \Pantheion\Database\Table\Table use(string $table)
+ * @method static \Pantheion\Database\Table\Table[] all(bool $pivots, array $except)
+ * @method static void drop(string $table)
+ * 
+ */
 class Table extends Facade
 {
     protected static function accessor()
